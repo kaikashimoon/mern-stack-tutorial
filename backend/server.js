7423848be1c59052590b5354bb7bc0f6ade29fd5
kai <leonc4667@gmail.com>
2022-06-03 09:41:31 -0400

    finish and ddeploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const cors = require("cors")
 
 connectDB()
 
@@ -11,6 +14,12 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
